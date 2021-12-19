279ffd529c2194177cabace987a58803b314250e
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-12-19 17:09:17 +0300

    test

---
+++ commands/game.js
@@ -0,0 +1,24 @@
+const { Command_template } = require("../config/templates");
+const Discord = require("discord.js");
+
+class Command extends Command_template {
+  constructor(args, interaction) {
+    super(interaction);
+    Object.assign(this, args);
+
+    this.options = {
+      permissions: [],
+      custom_perms: [],
+      slash: {
+        name: "game",
+        description: "COMMAND_DESCRIPTION",
+      },
+    };
+  }
+
+  async execute() {
+    this.db = this.mongo.db("gtaEZ");
+  }
+}
+
+module.exports = Command;
