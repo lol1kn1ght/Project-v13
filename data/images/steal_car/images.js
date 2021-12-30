4a68b048f09e85765eb009c56d78f28448891200
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-12-30 12:31:59 +0300

    d

--- data/images/steal_car/images.js
+++ data/images/steal_car/images.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 
 let readdir = dir =>
-  new Promise(resolve => fs.readdir(dir, (err, data) => resolve(data)));
+  new Promise(resolve =>
+    fs.readdir(dir, (err, data) =>
+      resolve(data.map(image_file => dir + "\\" + image_file))
+    )
+  );
 
 let router = new Proxy(
   {},
   {
     get(target, prop) {
       try {
-        return readdir(__dirname + "/" + prop);
+        return readdir(__dirname + "\\" + prop);
       } catch (e) {
         return undefined;
       }
