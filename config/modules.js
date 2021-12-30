ef87e79542f7727d4cc540a8aa7b769e110aa09f
loli-knight <60938098+KnightDark-loli@users.noreply.github.com>
2021-12-30 12:25:58 +0300

    d

--- config/modules.js
+++ config/modules.js
@@ -1,5 +1,17 @@
 module.exports = {
   config: require("./config.json"),
   random: require("../functions/random"),
-  Profile: require("../functions/profile")
+  Profile: require("../functions/profile"),
+  time: require("../functions/msToTime"),
+  images: {
+    steal_car: require("../data/images/steal_car/images.js"),
+    default: require("../data/images/default/images.json")
+  },
+  games: {
+    Steal_car: require("../data/modules/steal_car")
+  },
+  dialogues: (() => {
+    let config = require("./config.json");
+    return require(`../data/dialogues/${config.language}.json`);
+  })()
 };
